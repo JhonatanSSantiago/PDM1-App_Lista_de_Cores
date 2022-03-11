646d58782e6b0efa05e15ad2dc5395a2af00bb07
Jhonatan Santiago <jhonssantiago@gmail.com>
2022-03-11 11:07:42 -0300

    Criando metodos para mostrar a caixa de dialogo

--- app/src/main/java/com/jhonssantiago/listadecores/MainActivity.java
+++ app/src/main/java/com/jhonssantiago/listadecores/MainActivity.java
@@ -3,12 +3,29 @@ package com.jhonssantiago.listadecores;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openAlertDialog();
+            }
+        });
+    }
+
+
+    public void openAlertDialog(){
+        MyDialogFragment mdf = new MyDialogFragment();
+        mdf.show(getSupportFragmentManager(), "clima");
     }
 }
